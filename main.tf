91207669857aa558510eb1f4d1c490223d4ad5d0
tettehnarh <leslienarh@outlook.com>
2023-08-03 11:18:15 +0000

    Configure CloudWatch log group for Lambda function

--- main.tf
+++ main.tf
@@ -117,3 +117,8 @@ resource "aws_s3_bucket_notification" "thumbnail_notification" {
   depends_on = [aws_lambda_permission.thumbnail_allow_bucket]
 }
 
+# Create a CloudWatch log group for the Lambda function
+resource "aws_cloudwatch_log_group" "thumbnail_cloudwatch" {
+  name             = "/aws/lambda/${aws_lambda_function.thumbnail_lambda.function_name}"
+  retention_in_days = var.log_group_retention_days
+}
